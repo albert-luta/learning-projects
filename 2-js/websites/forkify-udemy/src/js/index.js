54f01377519e3ca47e5a88acc1da949efc7cef61
Albert Luta <luta.albert100@gmail.com>
2019-12-27 04:52:35 +0200

    js/websites/forkify Implemented Search/searchView modules under models/views + created base.js for the dom queries

---
+++ 2-js/websites/forkify-udemy/src/js/index.js
@@ -0,0 +1,32 @@
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
+
+/** Global state of the app
+ *  - Search object
+ *  - Current recipe object
+ *  - Shopping List object
+ *  - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+	const query = searchView.getInput();
+
+	if (query) {
+		state.search = new Search(query);
+
+		searchView.clearInput();
+		searchView.clearResults();
+
+		await state.search.getResults();
+
+		searchView.renderResults(state.search.result);
+	}
+};
+
+elements.searchForm.addEventListener("submit", e => {
+	e.preventDefault();
+
+	controlSearch();
+});
---
+++ 2-js/websites/forkify-udemy/src/js/views/searchView.js
@@ -0,0 +1,51 @@
+import { elements } from "./base";
+
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => {
+	elements.searchInput.value = "";
+};
+
+export const clearResults = () => {
+	elements.searchResList.innerHTML = "";
+};
+
+const limitRecipeTitle = (title, limit = 17) => {
+	if (title.length > limit) {
+		const newTitle = [];
+
+		title.split(" ").reduce((acc, curr) => {
+			if (acc + curr.length <= limit) {
+				newTitle.push(curr);
+			}
+
+			return acc + curr.length;
+		}, 0);
+
+		return `${newTitle.join(" ")} ...`;
+	}
+
+	return title;
+};
+
+const renderRecipe = recipe => {
+	const markup = `
+		<li>
+			<a class="results__link" href="#${recipe.recipe_id}">
+				<figure class="results__fig">
+					<img src="${recipe.image_url}" alt="${recipe.title}">
+				</figure>
+				<div class="results__data">
+					<h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+					<p class="results__author">${recipe.publisher}</p>
+				</div>
+			</a>
+        </li>
+	`;
+
+	elements.searchResList.insertAdjacentHTML("beforeend", markup);
+};
+
+export const renderResults = recipes => {
+	recipes.forEach(renderRecipe);
+};
