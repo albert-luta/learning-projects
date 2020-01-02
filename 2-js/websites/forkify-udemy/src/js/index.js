9e4339b0aded212290b59529c5767a70b8fd5654
Albert Luta <luta.albert100@gmail.com>
2020-01-02 20:21:02 +0200

    js/websites/forkify Completed the Recipe view(most parts)

--- 2-js/websites/forkify-udemy/src/js/index.js
+++ 2-js/websites/forkify-udemy/src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import * as searchView from "./views/searchView";
+import * as recipeView from "./views/recipeView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 /** Global state of the app
@@ -53,6 +54,11 @@ const controlRecipe = async () => {
 	const id = window.location.hash.replace("#", "");
 
 	if (id) {
+		recipeView.clearRecipe();
+		renderLoader(elements.recipe);
+
+		if (state.search) searchView.highlightSelected(id);
+
 		state.recipe = new Recipe(id);
 
 		try {
@@ -61,6 +67,9 @@ const controlRecipe = async () => {
 
 			state.recipe.calcTime();
 			state.recipe.calcServings();
+
+			clearLoader();
+			recipeView.renderRecipe(state.recipe);
 		} catch (error) {
 			console.log(error);
 		}
@@ -68,3 +77,17 @@ const controlRecipe = async () => {
 };
 
 ["hashchange", "load"].forEach(event => window.addEventListener(event, controlRecipe));
+
+elements.recipe.addEventListener("click", e => {
+	if (e.target.matches(".btn-decrease, .btn-decrease *")) {
+		if (state.recipe.servings > 1) {
+			state.recipe.updateServings("dec");
+			recipeView.updateServingsIngredients(state.recipe);
+		}
+	} else if (e.target.matches(".btn-increase, .btn-increase *")) {
+		state.recipe.updateServings("inc");
+		recipeView.updateServingsIngredients(state.recipe);
+	}
+
+	console.log(state.recipe);
+});
--- 2-js/websites/forkify-udemy/src/js/models/Recipe.js
+++ 2-js/websites/forkify-udemy/src/js/models/Recipe.js
@@ -33,6 +33,7 @@ export default class Recipe {
 	parseIngredients() {
 		const unitsLong = ["tablespoons", "tablespoon", "ounces", "ounce", "teaspoons", "teaspoon", "cups", "pounds"];
 		const unitsShort = ["tbsp", "tbsp", "oz", "oz", "tsp", "tsp", "cup", "pound"];
+		const units = [...unitsShort, "kg", "g"];
 
 		const newIngredients = this.ingredients.map(el => {
 			let ingredient = el.toLowerCase();
@@ -43,7 +44,7 @@ export default class Recipe {
 			ingredient = ingredient.replace(/ *\([^)]*\) */g, " ");
 
 			const arrIng = ingredient.split(" ");
-			const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+			const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
 			let objIng;
 			if (unitIndex > -1) {
@@ -78,5 +79,15 @@ export default class Recipe {
 		});
 
 		this.ingredients = newIngredients;
+	}
+
+	updateServings(type) {
+		const newServings = type === "dec" ? this.servings - 1 : this.servings + 1;
+
+		this.ingredients.forEach(ing => {
+			ing.count *= newServings / this.servings;
+		});
+
+		this.servings = newServings;
 	}
 }
