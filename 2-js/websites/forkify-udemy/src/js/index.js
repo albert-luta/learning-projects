15fdd5bccb8e1ce0cce09473b099e72d4c32dea9
Albert Luta <luta.albert100@gmail.com>
2020-01-02 22:15:42 +0200

    js/websites/forkify Completed the controllor for the Shopping List

--- 2-js/websites/forkify-udemy/src/js/index.js
+++ 2-js/websites/forkify-udemy/src/js/index.js
@@ -3,6 +3,8 @@ import Recipe from "./models/Recipe";
 import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
+
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 /** Global state of the app
@@ -12,6 +14,7 @@ import { elements, renderLoader, clearLoader } from "./views/base";
  *  - Liked recipes
  */
 const state = {};
+window.state = state;
 
 const controlSearch = async () => {
 	const query = searchView.getInput();
@@ -79,6 +82,29 @@ const controlRecipe = async () => {
 
 ["hashchange", "load"].forEach(event => window.addEventListener(event, controlRecipe));
 
+const controlList = () => {
+	if (!state.list) state.list = new List();
+
+	state.recipe.ingredients.forEach(el => {
+		const item = state.list.addItem(el.count, el.unit, el.ingredient);
+
+		listView.renderItem(item);
+	});
+};
+
+elements.shopping.addEventListener("click", e => {
+	const id = e.target.closest(".shopping__item").dataset.itemid;
+
+	if (e.target.matches("shopping__delete, .shopping__delete *")) {
+		state.list.deleteItem(id);
+		listView.deleteItem(id);
+	} else if (e.target.matches(".shopping__count-value")) {
+		const val = parseFloat(e.target.value);
+
+		state.list.updateCount(id, val);
+	}
+});
+
 elements.recipe.addEventListener("click", e => {
 	if (e.target.matches(".btn-decrease, .btn-decrease *")) {
 		if (state.recipe.servings > 1) {
@@ -88,6 +114,8 @@ elements.recipe.addEventListener("click", e => {
 	} else if (e.target.matches(".btn-increase, .btn-increase *")) {
 		state.recipe.updateServings("inc");
 		recipeView.updateServingsIngredients(state.recipe);
+	} else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
+		controlList();
 	}
 });
 
--- 2-js/websites/forkify-udemy/src/js/views/listView.js
+++ 2-js/websites/forkify-udemy/src/js/views/listView.js
@@ -4,7 +4,7 @@ export const renderItem = item => {
 	const markup = `
 		<li class="shopping__item" data-itemid=${item.id}>
 			<div class="shopping__count">
-				<input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value>
+				<input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
 				<p>${item.unit}</p>
 			</div>
 			<p class="shopping__description">${item.ingredient}</p>
@@ -20,6 +20,6 @@ export const renderItem = item => {
 };
 
 export const deleteItem = id => {
-	const item = document.querySelector(`[date-itemid="${id}"]`);
-	item.parentElement.removeChild(item);
+	const item = document.querySelector(`[data-itemid="${id}"]`);
+	if (item) item.parentElement.removeChild(item);
 };
